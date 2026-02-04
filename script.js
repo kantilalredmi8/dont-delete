0d24bbeeb0583e6730776410c294064d8dfcfae0
kantilalredmi8 <kantilalredmi8@gmail.com>
2026-02-05 00:19:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,27 @@
 function login() {
+
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
 
   if (email === "karitik47@gmail.com" && password === "ritikkll") {
     window.location = "profile.html";
-  } else {
+  }
+  else {
     alert("Wrong email or password");
   }
 }
 
-let currentScreen = "";
-
-window.onload = function () {
-  if (document.body.classList.contains("profile")) {
-    currentScreen = "profile";
-  } else if (document.body.classList.contains("menu-page")) {
-    currentScreen = "menu";
-    showScreen("menu");
-  }
-};
-
-function showScreen(screenName) {
-  document.body.style.backgroundImage =
-    url("images/${screenName}.jpg");
-  currentScreen = screenName;
+// Step 2 → Profile → Menu
+function goMenu() {
+  window.location = "menu.html";
 }
 
-document.addEventListener("click", function (e) {
+// Step 3 → Menu → Your Photo
+function openMyPhoto() {
+  window.location = "me.html";
+}
 
-  if (currentScreen === "profile") {
-    if (e.clientX < 80 && e.clientY < 80) {
-      window.location = "menu.html";
-    }
-  }
-
-  else if (currentScreen === "menu") {
-    showScreen("results-list");
-  }
-
-  else if (currentScreen === "results-list") {
-    showScreen("final-result");
-  }
-
-});
+// Step 4 → Your Photo → Father Photo
+function openFatherPhoto() {
+  window.location = "father.html";
+}
