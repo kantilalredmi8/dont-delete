cb01d71ab362c7e895237e5865e4ceca84cee99f
kantilalredmi8 <kantilalredmi8@gmail.com>
2026-02-04 23:54:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-function login(){
+function login() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
 
-  if(email === "karitik47@gmail.com" && password === "ritikkll"){
+  if (email === "karitik47@gmail.com" && password === "ritikkll") {
     window.location = "profile.html";
   } else {
-    alert("Wrong Email or Password");
+    alert("Wrong email or password");
   }
 }
+
+/* ----------------------------
+   SCREEN SYSTEM AFTER MENU
+-----------------------------*/
+
+// Track current screen
+let currentScreen = "menu";
+
+// Change background image
+function showScreen(screenName) {
+  document.body.style.backgroundImage = url("images/${screenName}.jpg");
+  currentScreen = screenName;
+}
+
+// Click anywhere to open next image
+document.addEventListener("click", function () {
+
+  // 3rd Image → Menu
+  if (currentScreen === "menu") {
+    showScreen("results-list");   // 4th Image
+  }
+
+  // 4th Image → Results List
+  else if (currentScreen === "results-list") {
+    showScreen("final-result");  // 5th Image
+  }
+
+  // 5th Image → Stop
+  else if (currentScreen === "final-result") {
+    return;
+  }
+
+});
